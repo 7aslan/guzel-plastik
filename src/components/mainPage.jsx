34f6fd5f5ddfb2f7c117c6e447f179e7d92133f0
Samet Aslan <sametaslan1710@gmail.com>
2025-01-26 19:40:00 +0300

    fix: centeralize of title and explanation of mainPage.jsx

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -1,10 +1,10 @@
 const MainPage = () => {
   return (
-    <div>
-      <h2 className="text-4xl font-bold mb-5 mt-8">
-        Siz hayal edin, biz gerçekleştirelim
+    <div className="flex flex-col items-center justify-center ">
+      <h2 className="text-4xl font-bold mb-5 mt-8 text-center">
+        Siz hayal edin biz gerçekleştirelim
       </h2>
-      <p className="max-w-md text-xl">
+      <p className="max-w-md text-xl text-center">
         Plastik ve çelik kalıp endüstrisinde yılların tecrübesiyle kaliteli ve çevre dostu
         çözümler sunuyoruz. 
       </p>
