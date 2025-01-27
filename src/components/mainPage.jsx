d8ab2d212666b1d4742984b8e7d771bd182d9f2e
Samet Aslan <103839899+7aslan@users.noreply.github.com>
2025-01-27 12:31:45 +0300

    Update mainPage.jsx
    Title was changed

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -2,7 +2,7 @@ const MainPage = () => {
   return (
     <div className="flex flex-col items-center justify-center ">
       <h2 className="text-4xl font-bold mb-5 mt-8 text-center">
-        Siz hayal edin biz gerçekleştirelim
+        Plastiğin Gücünü Keşfedin
       </h2>
       <p className="max-w-md text-xl text-center">
         Plastik ve enjeksiyon kalıp endüstrisinde yılların tecrübesiyle kaliteli ve çevre dostu
