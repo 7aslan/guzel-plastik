1d4ff1a4e9ca62c3f707db84510b7e45dcdc586b
Samet Aslan <sametaslan1710@gmail.com>
2025-01-27 17:38:27 +0300

    change: urunler data

--- src/data/urunler.js
+++ src/data/urunler.js
@@ -1,26 +1,163 @@
 const urunler = [
   {
     id: 1,
-    name: "Ürün 1",
-    image: "urun1_bdlkvk",
-    description: "gebelik testi",
+    name: "Rashing Halkası",
+    image: "urun1_qe63pq",
+    description:
+      "PP malzemeden imal edilmiştir. Geri dönüşüm ve orijinal olmak üzere çeşitlerimiz mevcuttur.",
   }, // Cloudinary public_id
   {
     id: 2,
-    name: "Ürün 2",
-    image: "urun9_p6woqo.jpg",
-    description: "bigudi dolgu malzemesi",
+    name: "Mühürleme Aparatı (Mavi)",
+    image: "urun2_lw0pkq",
+    description: "1. sınıf ABS malzemeden imal edilmektedir.",
   },
-  { id: 3, name: "Ürün 3", image: "kelepceson_kojv3n", description: "" },
-  { id: 4, name: "Ürün 4", image: "urun6_gqm26d", description: "" },
-  { id: 5, name: "Ürün 5", image: "urun7_xgmwg6", description: "" },
-  { id: 6, name: "Ürün 6", image: "urun3_qw1lnw", description: "" },
-  { id: 7, name: "Ürün 7", image: "urun4_k4egd8", description: "" },
-  { id: 8, name: "Ürün 8", image: "318120996_684932476667786_6697321402512040339_n_hilq2q", description: "kırmızı kelepce" },
-  { id: 9, name: "Ürün 9", image: "conta_zdwofj", description: "kör conta" },
-  { id: 10, name: "Ürün 10", image: "urun10_vsxxmk", description: "bıçak sapı" },
+  {
+    id: 3,
+    name: "Mühürleme Aparatı (Kırmızı)",
+    image: "urun3_aymagd",
+    description:
+      "1. sınıf ABS malzemeden imal edilmektedir. Kaçak kullanım tespiti halinde kullanılır.",
+  },
+  {
+    id: 4,
+    name: "Su Kesme Aparatı",
+    image: "urun4_lqacun",
+    description:
+      "1. sınıf PP malzemeden imal edilmektedir. Kaçak kullanım tespiti halinde kullanılır.",
+  },
+  {
+    id: 5,
+    name: "Su Kesme Aparatı",
+    image: "urun5_tlxdpj",
+    description:
+      "Küresel vanalar için firmamız tarafından geliştirilmiş bir üründür.",
+  },
+  {
+    id: 6,
+    name: "Helezon Tip Taşıma Makinesi Paleti",
+    image: "urun6_leyvta",
+    description:
+      "1. sınıf polyemit malzemeden üretilmiştir. Çeşitli renk seçenekleri mevcuttur.",
+  },
+  { id: 7, name: "Klor Emme Sepeti", image: "urun7_lwnfgh", description: "" },
+  {
+    id: 8,
+    name: "Sedye Pedalı",
+    image: "urun8_xrt6nl",
+    description: "PP den üretilmiştir. Renk seçenekleri mevcuttur.",
+  },
+  {
+    id: 9,
+    name: "Polyemit Dişlisi",
+    image: "urun9_q4fwie",
+    description: "Polyemit Dişli",
+  },
+  {
+    id: 10,
+    name: "Kaplin",
+    image: "urun10_wtx9yx",
+    description: "Motor ve Pompayı birbirine bağlar.",
+  },
+  {
+    id: 11,
+    name: "Doğalgaz Mühürleme Aparatı",
+    image: "urun11_x2npvz",
+    description:
+      "Doğalgaz saatinin birleşim yerlerine bağlanmak için üretilmiştir.",
+  },
+  {
+    id: 12,
+    name: "EKO Aparat",
+    image: "urun12_unznoi",
+    description: "ABS mühürleme aparatının ekonomik ve tasarruflu ürünüdür.",
+  },
+  {
+    id: 13,
+    name: "Raylı Kapı Denge Plastiği",
+    image: "urun13_msldc8",
+    description: "",
+  },
+  {
+    id: 14,
+    name: "Serum Askı Yuvası",
+    image: "urun14_e6zkuu",
+    description: "Vidalı Serum askılarının boy ayarı için üretilmiştir. ",
+  },
+  {
+    id: 16,
+    name: "Elektrik Abone Plakası",
+    image: "urun16_rhk3rj",
+    description: "",
+  },
+  {
+    id: 17,
+    name: "Su Abone Plakası",
+    image: "urun17_zjykmj",
+    description: "",
+  },
+  {
+    id: 18,
+    name: "Tahlil Kutusu",
+    image: "urun18_t1wjgb",
+    description: "",
+  },
+  {
+    id: 19,
+    name: "Araba Farı Yedek Parça",
+    image: "urun19_cjcnhk",
+    description: "",
+  },
+  {
+    id: 20,
+    name: "DN-25 Conta",
+    image: "urun20_df9fcw",
+    description: "Su saati contası",
+  },
+  
+  {
+    id: 23,
+    name: "Mil Başlığı",
+    image: "urun23_map9jk",
+    description: "10 mm miller için üretilmiştir. ",
+  },
+  {
+    id: 25,
+    name: "Derz Dolgu Düzleyici",
+    image: "urun25_jsgn9p",
+    description: "",
+  },
+  {
+    id: 26,
+    name: "Sedye Tamponu",
+    image: "urun26_owq7tl",
+    description: "PP den üretilmiştir. Renk seçenekleri mevcuttur. ",
+  },
+  {
+    id: 27,
+    name: "M-8 Kelebek Vida",
+    image: "urun27_lc3qhx",
+    description: "Metrik 8 vida üzerine plastik dolgu. Anahtarsız kullanım için üretilmiştir.",
+  },
+  {
+    id: 28,
+    name: "Gebelik Testi",
+    image: "urun28_vumpic",
+    description: "",
+  },
+  {
+    id: 29,
+    name: "PVS Teker",
+    image: "urun29_rkbm7c",Fayans
+    description: "",
+  },
+  {
+    id: 30,
+    name: "Klor Enjektörü",
+    image: "urun30_xrk2wu",
+    description: "Suya klor basması için üretilmiştir.",
+  },
   // Yeni ürünler eklemek için sadece burayı düzenlemek yeterli
 ];
 
 export default urunler;
-
